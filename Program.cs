94a84ce56bd66b1ae88d2928ca9bff312b036757
MaidInstance <thereis.dev@outlook.com>
2025-01-07 09:27:49 +0800

    加入配置文件，可以方便的更改存放图片的文件夹名称，目前可能存在bug，尚未测试

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-﻿using System.Text.RegularExpressions;
+﻿using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace MarkdownHelper;
 
 internal class Program
 {
     private static string 存放图片的文件夹 = "Photos";
+    private static readonly string ConfigFileLocation = "./mh_config.json";
 
-    private static string 当前程序所在文件文件夹路径 = AppDomain.CurrentDomain.BaseDirectory;
+    private static readonly string 当前程序所在文件文件夹路径 = AppDomain.CurrentDomain.BaseDirectory;
     private static string 程序所在文件夹内存放图片的文件夹的路径 = Path.Combine(当前程序所在文件文件夹路径, 存放图片的文件夹);
 
     private static void Main(string[] args)
     {
+        bool hasCommandLineArgs = args.Length > 0;
+
         if ((!Directory.Exists(程序所在文件夹内存放图片的文件夹的路径))) Directory.CreateDirectory(程序所在文件夹内存放图片的文件夹的路径);
         string[] files = Directory.GetFiles(当前程序所在文件文件夹路径);
-        List<string> md_files = new();
+        List<string> md_files = [];
 
-        if (args.Length > 0)
+        if (hasCommandLineArgs)
         {
             foreach (var 文件地址 in args)
             {
@@ -28,10 +34,41 @@ internal class Program
             foreach (string file in files) if (file.EndsWith(".md")) md_files.Add(file);
         }
 
+        // 配置文件是否存在，如果不存在使用默认配置执行
+        JObject? j = null;
+        if (!File.Exists(ConfigFileLocation))
+        {
+            j = JObject.Parse(File.ReadAllText(ConfigFileLocation));
+        }
+        if (j is null)
+        {
+            JustDoIt(md_files);
+            return;
+        }
+
+        LoadConfig(j, out bool isDefault, out bool folderIsChanged, out string folderName, out string folderName_);
+
+        // 如果配置文件存在错误不能正确读取值，就默认执行
+        if (isDefault) JustDoIt(md_files);
+        // 使用配置文件的文件夹名字复制给程序，后面的执行就不会是默认执行
+        UseNewSettings(folderName);
+        JustDoIt(md_files);
+        if (folderIsChanged)
+        {
+            var oldFolderPath = Path.Combine(当前程序所在文件文件夹路径, folderName_);
+            if (Path.Exists(oldFolderPath)) Directory.Delete(oldFolderPath, true);
+            j["FolderName_bak"] = folderName;
+
+            File.WriteAllText(ConfigFileLocation, Convert.ToString(j));
+        }
+    }
+
+    private static void JustDoIt(List<string> md_files)
+    {
         foreach (var md in md_files)
         {
             Console.WriteLine($"FILE: {md} ");
-            var 当前文件名_带后缀 = Path.GetFileName(md);
+            // var 当前文件名_带后缀 = Path.GetFileName(md);
             var 当前文件名_不带后缀 = Path.GetFileNameWithoutExtension(md);
             var 当前文件图片迁移的目标文件夹 = Path.Combine(程序所在文件夹内存放图片的文件夹的路径, 当前文件名_不带后缀);
 
@@ -40,11 +77,11 @@ internal class Program
             if (!Directory.Exists(当前文件图片迁移的目标文件夹)) Directory.CreateDirectory(当前文件图片迁移的目标文件夹);
 
             获取文件中的特定链接(out List<string> 当前文件的图片链接列表, md);
-            // 获取文件中所有的图片超链接，然后对每个链接进行替换
 
+            // 获取文件中所有的图片超链接，然后对每个链接进行替换
             foreach (var 单个链接 in 当前文件的图片链接列表)
             {
-                if (isWebUrl(单个链接)) continue; // 或许有用吧，我不知道
+                if (IsWebUrl(单个链接)) continue; // 或许有用吧，我不知道
                 复制图片并返回新的图片超链接(单个链接, 当前文件图片迁移的目标文件夹, 当前文件名_不带后缀, out string 新的图片超链接);
                 当前文件的所有内容 = 当前文件的所有内容.Replace(单个链接, 新的图片超链接);
             }
@@ -52,17 +89,76 @@ internal class Program
         }
     }
 
-    private static bool isWebUrl(string url)
+    private static bool IsFolderNameValid(string folderName)
+    {
+        if (folderName.Length > 128)
+        {
+            return false;
+        }
+
+        string illegalPattern = @"[\\/:\*\?""<>|]";
+        Regex regex = new(illegalPattern);
+        if (regex.IsMatch(folderName))
+        {
+            return false;
+        }
+
+        if (folderName.StartsWith(' ') || folderName.StartsWith('.') ||
+            folderName.EndsWith(' ') || folderName.EndsWith('.'))
+        {
+            return false;
+        }
+
+        if (string.IsNullOrEmpty(folderName))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void UseNewSettings(string folderName)
+    {
+        存放图片的文件夹 = folderName;
+        程序所在文件夹内存放图片的文件夹的路径 = Path.Combine(当前程序所在文件文件夹路径, 存放图片的文件夹);
+    }
+
+    private static void LoadConfig(JObject config, out bool defaultMode, out bool folderIsChanged, out string newFolderName, out string oldFolderName)
+    {
+        defaultMode = true;
+        folderIsChanged = false;
+        newFolderName = 存放图片的文件夹;
+        oldFolderName = 存放图片的文件夹;
+        try
+        {
+            if (config["FolderName"] is not null && config["FolderName_bak"] is not null)
+            {
+                var folderName_ = config["FolderName"].ToString();
+                var folderName_bak = config["FolderName_bak"].ToString();
+                defaultMode = false;
+                if (IsFolderNameValid(folderName_)) newFolderName = folderName_;
+                if (IsFolderNameValid(folderName_bak)) oldFolderName = folderName_bak;
+                folderIsChanged = !folderName_.Equals(folderName_bak);
+            }
+            else defaultMode = true;
+        }
+        catch
+        {
+            Debug.WriteLine("配置文件错误");
+        }
+    }
+
+    private static bool IsWebUrl(string url)
     {
         return url.StartsWith("https") || url.StartsWith("http");
     }
 
     private static void 获取文件中的特定链接(out List<string> 图片的链接列表, string 文件名)
     {
-        图片的链接列表 = new();
+        图片的链接列表 = [];
         string markdownContent = File.ReadAllText(文件名);
         string pattern = @"!\[.*?\]\(.*?\)";
-        Regex regex = new Regex(pattern);
+        Regex regex = new(pattern);
         MatchCollection matches = regex.Matches(markdownContent);
 
         foreach (Match match in matches)
